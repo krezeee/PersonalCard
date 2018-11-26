05166706286a1d5fb752f28283f55a1b0240edd0
krezeee <pawel.krezel@gmail.com>
2018-11-26 20:09:28 +0100

    Updates

---
+++ precache-manifest.f6b51c69e2a563aadcc4aae90e280f11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b30e389e6d30166f849833dcdf19b1ba",
+    "url": "/PersonalCard/static/media/wavy-dots.b30e389e.png"
+  },
+  {
+    "revision": "36ea93d29350231aef969d9466315c00",
+    "url": "/PersonalCard/static/media/prescient.36ea93d2.jpg"
+  },
+  {
+    "revision": "2a47cecc58e1132e45461c4f4dcf9e7c",
+    "url": "/PersonalCard/static/media/MyPhoto.2a47cecc.jpg"
+  },
+  {
+    "revision": "b6838c3dc15a5340bad0",
+    "url": "/PersonalCard/static/js/runtime~main.b6838c3d.js"
+  },
+  {
+    "revision": "e2868e5eeb3a475c0b32",
+    "url": "/PersonalCard/static/js/main.e2868e5e.chunk.js"
+  },
+  {
+    "revision": "f5ec684af987eda99be1",
+    "url": "/PersonalCard/static/js/1.f5ec684a.chunk.js"
+  },
+  {
+    "revision": "e2868e5eeb3a475c0b32",
+    "url": "/PersonalCard/static/css/main.c3686a04.chunk.css"
+  },
+  {
+    "revision": "0223bdd31903e5cdb4c65132a9e94f87",
+    "url": "/PersonalCard/index.html"
+  }
+];
